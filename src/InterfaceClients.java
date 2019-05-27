5ba07d6d8d29e3f8175c974d37c18fd8cbe2569a
Cédric <cedric.plantet@gmail.com>
2019-05-27 15:25:40 +0200

    Interface Client Vide (Structure de Base)

--- src/InterfaceClients.java
+++ src/InterfaceClients.java
@@ -1,2 +1,21 @@
-public class InterfaceClients {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class InterfaceClients extends JFrame{
+
+    public InterfaceClients(){
+
+        JFrame fenetre = new JFrame();
+        fenetre.setBounds(350, 100, 700, 500);
+        fenetre.setResizable(false);
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenetre.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        InterfaceClients fenetreClients = new InterfaceClients();
+    }
+
 }
