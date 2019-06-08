1e8c06dc8117612ebf82ede4e46fea0b7434878c
louis-sama <louisleg29@gmail.com>
2019-06-08 21:32:08 +0200

    interface ajout loc avance

--- src/InterfaceAjoutLocationAvance.java
+++ src/InterfaceAjoutLocationAvance.java
@@ -8,46 +8,64 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.EventObject;
-/*
-public class InterfaceAjoutLocationAvance extends JFrame implements ActionListener{
+
+public class InterfaceAjoutLocationAvance extends JFrame implements ActionListener {
 
     private JButton ajout;
     private JButton annuler;
     private JComboBox vehiculeJComboBox;
     private JTextField dateDebut;
     private JTextField dateFin;
+    private JFrame fenetre;
 
-    public InterfaceAjoutLocationAvance(){
-
+    public InterfaceAjoutLocationAvance() {
+        fenetre = new JFrame();
         vehiculeJComboBox = new JComboBox();
-
-        ajout = new JButton("Ajout");
-
-
-
-        JLabel prixLoc = new JLabel("prixLoc");
-        JLabel nbPlace = new JLabel("nbPlace");
 
         ajout = new JButton("Ajouter");
         ajout.addActionListener(this);
+
+        JLabel dateDebut1 = new JLabel("dateDebut");
+        JLabel dateFin1 = new JLabel("dateFin");
+        JPanel panAjout = new JPanel();
+
+        dateDebut = new JTextField();
+        dateFin = new JTextField();
+
         annuler = new JButton("Annuler");
         annuler.addActionListener(this);
-        GridLayout grilleInfos = new GridLayout(9,2);
 
-        panInfos.setPreferredSize(new Dimension(700,600));
+        GridLayout grilleInfos = new GridLayout(3, 2);
+        panAjout.setLayout(grilleInfos);
 
-        panInfos.setLayout(grilleInfos);
-        panInfos.add(marque);
-        panInfos.add(textMarque);
-        panInfos.add(modele);
-        panInfos.add(textModele);
-        panInfos.add(vitesseMax);
-        panInfos.add(textVitesseMax);
-        panInfos.add(etat);
-        panInfos.add(textEtat);
+        panAjout.setLayout(grilleInfos);
 
+
+        panAjout.add(dateDebut1);
+        panAjout.add(dateDebut);
+        panAjout.add(dateFin1);
+        panAjout.add(dateFin);
+        panAjout.add(annuler);
+        panAjout.add(ajout);
+        fenetre.add(vehiculeJComboBox);
+        fenetre.add(panAjout);
+        fenetre.setVisible(true);
 
     }
 
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == ajout) {
+            fenetre.dispose();
+            InterfaceLocation interloc = new InterfaceLocation();
 
-}*/
+
+        }
+        if (e.getSource() == annuler) {
+            InterfaceAjoutLocation intajoutloc = new InterfaceAjoutLocation();
+            fenetre.dispose();
+
+
+        }
+    }
+}
+
