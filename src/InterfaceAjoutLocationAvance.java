1ee2b2520fd90acd431db4f332fd005bdb2c508a
louis-sama <louisleg29@gmail.com>
2019-06-08 21:12:11 +0200

    interface modifier avion

--- src/InterfaceAjoutLocationAvance.java
+++ src/InterfaceAjoutLocationAvance.java
@@ -1,2 +1,53 @@
-public class InterfaceAjoutLocationAvance {
-}
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.EventObject;
+/*
+public class InterfaceAjoutLocationAvance extends JFrame implements ActionListener{
+
+    private JButton ajout;
+    private JButton annuler;
+    private JComboBox vehiculeJComboBox;
+    private JTextField dateDebut;
+    private JTextField dateFin;
+
+    public InterfaceAjoutLocationAvance(){
+
+        vehiculeJComboBox = new JComboBox();
+
+        ajout = new JButton("Ajout");
+
+
+
+        JLabel prixLoc = new JLabel("prixLoc");
+        JLabel nbPlace = new JLabel("nbPlace");
+
+        ajout = new JButton("Ajouter");
+        ajout.addActionListener(this);
+        annuler = new JButton("Annuler");
+        annuler.addActionListener(this);
+        GridLayout grilleInfos = new GridLayout(9,2);
+
+        panInfos.setPreferredSize(new Dimension(700,600));
+
+        panInfos.setLayout(grilleInfos);
+        panInfos.add(marque);
+        panInfos.add(textMarque);
+        panInfos.add(modele);
+        panInfos.add(textModele);
+        panInfos.add(vitesseMax);
+        panInfos.add(textVitesseMax);
+        panInfos.add(etat);
+        panInfos.add(textEtat);
+
+
+    }
+
+
+}*/
